92b62b05ec9b87d83fd0f869c82371dc76aa27d8
quynh150105 <quynh1512005@gmail.com>
2025-06-19 16:06:15 +0700

    add base project

---
+++ hit-networking-base/src/main/java/com/example/hit_networking_base/HitNetworkingBaseApplication.java
@@ -0,0 +1,26 @@
+package com.example.hit_networking_base;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+@Slf4j
+@SpringBootApplication
+public class HitNetworkingBaseApplication {
+
+	public static void main(String[] args) {
+		Environment env = SpringApplication.run(HitNetworkingBaseApplication.class, args).getEnvironment();
+		String appName = env.getProperty("spring.application.name");
+		if (appName != null) {
+			appName = appName.toUpperCase();
+		}
+		String port = env.getProperty("server.port");
+		log.info("-------------------------START " + appName
+				+ " Application------------------------------");
+		log.info("   Application         : " + appName);
+		log.info("   Url swagger-ui      : http://localhost:" + port + "/swagger-ui.html");
+		log.info("-------------------------START SUCCESS " + appName
+				+ " Application------------------------------");
+	}
+
+}
---
+++ hit-networking-base/src/test/java/com/example/hit_networking_base/HitNetworkingBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hit_networking_base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HitNetworkingBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
