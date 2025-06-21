caa1f5436e85b9da560ace0e76fd1cd32168cb2e
Thit-Cho-Mam-Tom <thuongtuc2k1@gmail.com>
2025-06-21 21:48:14 +0700

    update entity

---
+++ hit-networking-base/src/main/java/com/example/hit_networking_base/domain/entity/User.java
@@ -0,0 +1,62 @@
+package com.example.hit_networking_base.domain.entity;
+
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @Column(name = "user_id")
+    private Integer userId;
+
+    @Column(length = 50, nullable = false)
+    private String username;
+
+    @Column(name = "password_hash", length = 255, nullable = false)
+    private String passwordHash;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(name = "full_name", length = 100)
+    private String fullName;
+
+    @Column(length = 100)
+    private String email;
+
+    @Column(length = 20)
+    private String phone;
+
+    @Column(name = "avatar_url", length = 255)
+    private String avatarUrl;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    @OneToMany(mappedBy = "user")
+    private List<CV> cvs;
+
+    @OneToMany(mappedBy = "user")
+    private List<Comment> comments;
+
+    @OneToMany(mappedBy = "user")
+    private List<Reaction> reactions;
+
+    @OneToMany(mappedBy = "creator")
+    private List<Event> createdEvents;
+
+    @OneToMany(mappedBy = "creator")
+    private List<JobPost> createdJobPosts;
+
+    public enum Role {
+        BQT, TV
+    }
+}
