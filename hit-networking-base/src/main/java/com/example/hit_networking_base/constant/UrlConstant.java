2cb25977f100760aa195815d90d8b556014fb993
quynh150105 <quynh1512005@gmail.com>
2025-07-02 16:24:37 +0700

    add UrlConstant for user

--- hit-networking-base/src/main/java/com/example/hit_networking_base/constant/UrlConstant.java
+++ hit-networking-base/src/main/java/com/example/hit_networking_base/constant/UrlConstant.java
@@ -1,11 +1,18 @@
 package com.example.hit_networking_base.constant;
 
+import org.springframework.security.access.method.P;
+
 public class UrlConstant {
     public static class Admin{
         public static final String PREFIX = "admin";
         public static final String CREATE = PREFIX + "/create";
         public static final String UPDATE = PREFIX + "/update";
         public static final String IMPORT = PREFIX + "/import";
+    }
 
+    public static class User{
+        public static final String PREFIX= "user";
+        public static final String UPDATE = PREFIX + "/update";s
+        public static final String SHOW = PREFIX + "/show";
     }
 }
